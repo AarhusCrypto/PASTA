5864ea9ab7f93231cfc486dc0ce4504a4ca698f1
Rasmus Winther Zakarias <rwl@cs.au.dk>
2015-05-18 08:57:43 +0200

    Zp and TheIntegers are implemented

--- common/math/src/ring.c
+++ common/math/src/ring.c
@@ -5,6 +5,11 @@
 #include <singlelinkedlist.h>
 
 List SingleLinkedList_new(OE oe);
+
+
+// ---------------------------------------------------
+// The Integers implementation based on GMP
+// ---------------------------------------------------
 typedef struct _the_ints_ {
 	OE oe;
 	List allocated_items; // List<RE>
@@ -242,7 +247,237 @@ void TheIntegers_Destroy(Ring * ring) {
 	oe->putmem(impl);
 }
 
+// ---------------------------------------------------
+// Zp implementation
+// ---------------------------------------------------
+static RE ZpE_New(Ring origin);
+typedef struct _zp_ {
+	OE oe;
+	List allocated_elms; // List<RE<ZpE>>
+	mpz_t * mod;
+} *Zp;
+
+static void ZpE_Destroy(RE * elm);
+void Zp_Destroy(Ring * ring) {
+	Ring r = 0;
+	uint i = 0;
+	Zp zp = 0;
+
+	if (!ring) return;
+
+	r = *ring;
+	if (!r) return;
+
+	zp = r->impl;
+	if (!zp) return;
+
+	COO_detach(r->from_cstr);
+	COO_detach(r->from_ull);
+	COO_detach(r->one);
+	COO_detach(r->zero);
+
+	for (i = 0; i < zp->allocated_elms->size(); ++i) {
+		RE c = zp->allocated_elms->get_element(i);
+		ZpE_Destroy(&c);
+	}
+	SingleLinkedList_destroy(&zp->allocated_elms);
+	mpz_clear(*zp->mod);
+	zp->oe->putmem(zp->mod);
+	zp->oe->putmem(zp);
+}
+
+typedef struct _zp_elm_ {
+	Ring origin;
+	mpz_t * elm;
+} *ZpE;
+
+COO_DEF(RE, RC, zp_add, RE other) {
+	ZpE impl = this->impl;
+	Ring org = impl->origin;
+	Zp zp = (Zp)org->impl;
+	OE oe = zp->oe;
+	ZpE ozpe = other->impl;
+	mpz_t * res = oe->getmem(sizeof(*res));
+
+	mpz_add(*res, *impl->elm, *ozpe->elm);
+	oe->putmem(impl->elm);
+	impl->elm = res;
+
+	return RC_OK;
+}}
+
+COO_DEF(RE, RE, zp_cpy) {
+	ZpE impl = this->impl;
+	Ring org = impl->origin;
+	Zp zp = (Zp)org->impl;
+	OE oe = zp->oe;
+	RE result = ZpE_New(org);
+	ZpE rzp = result->impl;
+
+	mpz_add_ui(*rzp->elm, *impl->elm, 0);
+
+	return result;
+}}
+
+COO_DEF(RE, RC, zp_mul, RE other) {
+	ZpE impl = this->impl;
+	Ring org = impl->origin;
+	Zp zp = (Zp)org->impl;
+	OE oe = zp->oe;
+	ZpE ozpe = other->impl;
+	mpz_t * res = oe->getmem(sizeof(*res));
+
+	mpz_mul(*res, *impl->elm, *ozpe->elm);
+	oe->putmem(impl->elm);
+	impl->elm = res;
+
+	return RC_OK;
+
+}}
+
+COO_DEF(RE, RC, zp_pow, RE other) {
+	ZpE impl = this->impl;
+	Ring org = impl->origin;
+	Zp zp = (Zp)org->impl;
+	OE oe = zp->oe;
+	ZpE ozpe = other->impl;
+	mpz_t * res = oe->getmem(sizeof(*res));
+
+	mpz_pow_ui(*res, *impl->elm, mpz_get_ui(*ozpe->elm));
+	oe->putmem(impl->elm);
+	impl->elm = res;
+
+	return RC_OK;
+
+}}
+
+COO_DEF(RE, RC, zp_powi,ull i) {
+	ZpE impl = this->impl;
+	Ring org = impl->origin;
+	Zp zp = (Zp)org->impl;
+	OE oe = zp->oe;
+	mpz_t * res = oe->getmem(sizeof(*res));
+
+	mpz_pow_ui(*res, *impl->elm, i);
+	oe->putmem(impl->elm);
+	impl->elm = res;
+
+	return RC_OK;
+
+}}
+
+static void set_zpelm_functions(RE re) {
+	re->add = COO_attach(re, RE_zp_add);
+	re->copy = COO_attach(re, RE_zp_cpy);
+	re->mul = COO_attach(re, RE_zp_mul);
+	re->pow = COO_attach(re, RE_zp_pow);
+	re->powi = COO_attach(re, RE_zp_powi);
+}
+
+static void ZpE_Destroy(RE * elm) {
+	RE r = 0;
+	OE oe = 0;
+	ZpE e = 0;
+	Zp zp = 0;
+
+	if (!elm) return;
+
+	r = *elm;
+	if (!r) return;
+
+	e = r->impl;
+	if (!e) return;
+
+	zp = e->origin->impl;
+	oe = zp->oe;
+
+	COO_detach(r->add);
+	COO_detach(r->copy);
+	COO_detach(r->mul);
+	COO_detach(r->pow);
+	COO_detach(r->powi);
+
+	mpz_clear(*e->elm);
+	oe->putmem(e->elm);
+	oe->putmem(e);
+	oe->putmem(r);
+}
+
+static RE ZpE_New(Ring origin) {
+	Zp zp = (Zp)origin->impl;
+	OE oe = zp->oe;
+	RE result = 0;
+	ZpE elm = 0; 
+
+	result = oe->getmem(sizeof(*result));
+	if (!result) goto failure;
+
+	elm = (ZpE)oe->getmem(sizeof(*elm));
+	if (!elm) goto failure;
+	
+	result->impl = elm;
+	set_zpelm_functions(result);
+	zp->allocated_elms->add_element(result);
+
+	return result;
+failure:
+	ZpE_Destroy(&result);
+	return 0;
+}
+
+COO_DEF(Ring, RE, zp_from_cstr, const char * cstr) {
+	Zp impl = (Zp)this->impl;
+	RE result = ZpE_New(this);
+	ZpE rzp = result->impl;
+
+	mpz_set_str(*rzp->elm, cstr,10);
+	return result;
+}}
+
+COO_DEF(Ring, RE, zp_from_ull, ull v) {
+	Zp impl = (Zp)this->impl;
+	RE result = ZpE_New(this);
+	ZpE rzp = result->impl;
+
+	mpz_set_ui(*rzp->elm, v);
+	return result;
+}}
+
+COO_DEF(Ring, RE, zp_one) {
+	Zp impl = (Zp)this->impl;
+	RE result = ZpE_New(this);
+	ZpE rzp = result->impl;
+
+	mpz_set_ui(*rzp->elm, 1);
+	return result;
+
+
+}}
+
+COO_DEF(Ring, RE, zp_zero) {
+	Zp impl = (Zp)this->impl;
+	RE result = ZpE_New(this);
+	ZpE rzp = result->impl;
+
+	mpz_set_ui(*rzp->elm, 0);
+	return result;
+
+}}
+
 Ring Zp_New(OE oe, RE p) {
 	Ring res = (Ring)oe->getmem(sizeof(*res));
-		return res;
+	Zp zp = (Zp)oe->getmem(sizeof(*zp));
+	TheIntsElm pi = (TheIntsElm)p->impl;
+
+	res->from_cstr = COO_attach(res, Ring_zp_from_cstr);
+	res->from_ull = COO_attach(res, Ring_zp_from_ull);
+	res->one = COO_attach(res, Ring_zp_one);
+	res->zero = COO_attach(res, Ring_zp_zero);
+	res->impl = zp;
+	zp->oe = oe;
+	zp->allocated_elms = SingleLinkedList_new(oe);
+	zp->mod = oe->getmem(sizeof(*zp->mod));
+	mpz_mul_ui(*zp->mod, *pi->gmpelm, 1);
+	return res;
 }
+
